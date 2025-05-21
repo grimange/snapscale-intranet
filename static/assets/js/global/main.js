b26338793dd123210ca0f1e5eb394d6029d09e88
grimange <ramjfive24@gmail.com>
2025-05-22 02:02:50 +0800

    Add dashboard_post_url property to main.js
    Introduce a new property, `dashboard_post_url`, to hold the URL for post requests. This ensures centralized and consistent management of endpoint URLs.
    
    Signed-off-by: grimange <ramjfive24@gmail.com>

--- static/assets/js/global/main.js
+++ static/assets/js/global/main.js
@@ -6,5 +6,6 @@ export class IntranetGlobal {
   constructor() {
       this.csrftoken = $("meta[name='csrf-token']").attr("content")
       this.user_profile_url = window.location.origin + "/employees/profile/"
+      this.dashboard_post_url = window.location.origin + "/post-request/"
   }
 }
